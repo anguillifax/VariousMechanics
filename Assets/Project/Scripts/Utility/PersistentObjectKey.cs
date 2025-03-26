536125679f9d8beef2cb0c1380c470aca82194cf
Bryan <10124208+anguillifax@users.noreply.github.com>
2025-03-26 13:23:12 -0700

    Import Scripts, Add Packages
    Import RGG utility scripts and clean up naming
    Import RGG persistent object guards and clean up naming
    Create core systems toolkit and reorganize existing scene objects
    Add Cinemachine, Serialization, and Logging packages

---
+++ Assets/Project/Scripts/Utility/PersistentObjectKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VariousMechanics
+{
+	[CreateAssetMenu(menuName = "Various Mechanics/Persistent Object Key")]
+	public class PersistentObjectKey : ScriptableObject
+	{
+		public enum DuplicationAction
+		{
+			DestroySelf,
+			DeactivateSelf,
+			UpdateBooleanOnly,
+		}
+
+		public DuplicationAction duplicationAction;
+	}
+}
