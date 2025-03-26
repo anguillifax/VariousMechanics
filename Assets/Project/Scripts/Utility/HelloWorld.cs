b74ec81ca20f357d7b0a143dd22288b795e51f60
Bryan <10124208+anguillifax@users.noreply.github.com>
2025-03-26 12:20:47 -0700

    Add Starter Files
    Add Unity project
    Add TextMeshPro Essentials
    Add InputSystem and disable legacy Input
    Create project folder
    Add scripts folder
    Update project settings and build settings

---
+++ Assets/Project/Scripts/Utility/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VariousMechanics
+{
+	public class HelloWorld : MonoBehaviour
+	{
+		private void Start()
+		{
+			Debug.Log("Project scripts are compiled");
+		}
+	}
+}
---
+++ Assets/Project/Scripts/Utility/ToggleFullscreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace VariousMechanics
+{
+	public class ToggleFullscreen : MonoBehaviour
+	{
+		private void Update()
+		{
+			if (Keyboard.current.f11Key.wasPressedThisFrame)
+			{
+				Screen.fullScreen = !Screen.fullScreen;
+			}
+		}
+	}
+}
